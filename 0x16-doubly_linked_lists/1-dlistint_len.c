61b5f43624460597fac6fe4c140f06ec560c6d03
Mikaelia <mikaelagurney@gmail.com>
2018-04-13 18:24:40 +0000

    first push

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *
+  *
+  *
+  *
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+	count = 0;
+
+	if (h == NULL)
+		return(0);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return(count);
+}
