4fa114ddfa68529087861434137fb5809bdf0b73
Mikaelia <mikaelagurney@gmail.com>
2018-04-20 04:06:51 +0000

    Add type condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (roman_string):
+    if (roman_string) and type(roman_string) is str:
         mylist = []
         mydict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
