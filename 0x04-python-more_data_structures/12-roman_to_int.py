4d903108241ca7512d5d20edd6fca8a79e2cea7b
Mikaelia <mikaelagurney@gmail.com>
2018-04-20 04:05:05 +0000

    Add task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (roman_string):
+        mylist = []
+        mydict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
+        for i, v in enumerate(roman_string):
+            if v in mydict:
+                mylist.append(mydict.get(v))
+                if v != "I" and roman_string[i - 1] is "I" and i > 0:
+                    mylist.append(-2)
+        return(sum(mylist))
+    return(0)
