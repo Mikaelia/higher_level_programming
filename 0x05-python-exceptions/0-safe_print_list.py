cf0274004039e78ee47213fa30a218c844270dc3
Mikaelia <mikaelagurney@gmail.com>
2018-04-30 19:13:47 +0000

    first push

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3:
+def safe_print_list(my_list=[], x=0):
+    num = 0;
+    try:
+        for a in range(x):
+            print(my_list[a], end = "")
+            num += 1
+    except IndexError:
+        print()
+        return num
+    else:
+        print()
+        return num
+
