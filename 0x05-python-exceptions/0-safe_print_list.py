c2de910beb47bf7d33ce823f358ba8561a5d31ee
Mikaelia <mikaelagurney@gmail.com>
2018-04-30 19:37:36 +0000

    Add pep8 fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3:
 def safe_print_list(my_list=[], x=0):
-    num = 0;
+    num = 0
     try:
         for a in range(x):
-            print(my_list[a], end = "")
+            print(my_list[a], end="")
             num += 1
     except IndexError:
         print()
@@ -11,4 +11,3 @@ def safe_print_list(my_list=[], x=0):
     else:
         print()
         return num
-
