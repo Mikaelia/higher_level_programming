4fab6cc4695ca7743bbb2b0152927a84c8d160a5
Mikaelia <mikaelagurney@gmail.com>
2018-05-02 00:29:32 +0000

    Add comments task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,14 +3,20 @@ class Square:
     """A square class"""
 
     def __init__(self, size=0):
+        """Initializes instance of Square
+        Args:
+            size (int): size of square
+        """
         self.size = size
 
     @property
     def size(self):
+        """gets size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """sets size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
