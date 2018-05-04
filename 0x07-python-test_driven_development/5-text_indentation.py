3a276d5878cc305feca2be82802c6161ee690567
Mikaelia <mikaelagurney@gmail.com>
2018-05-04 22:15:31 +0000

    Update 4 to test for whitespace

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,10 +8,16 @@ def text_indentation(text):
     Raises:
         TypeError: if text is not a string
     """
+    symbol = False
     if type(text) is not str:
         raise TypeError('text must be a string')
-    for i in range(len(text)):
-        print(text[i], end='')
-        if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print()
-            print()
+    for i, v in enumerate(text):
+        if v == ' ' and symbol == True:
+            pass
+        else:
+            symbol = False
+        if symbol == False:
+            print(v, end='')
+        if v in '.?:':
+            print('\n')
+            symbol = True
