0b024dab3c700090d1518a94b711e334a50011ef
Mikaelia <mikaelagurney@gmail.com>
2018-05-16 14:47:17 +0000

    change from isattribute to type

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-class BaseGeometry():
-    """base class"""
-    def area(self):
-        """area function"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Validates value"""
-        if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class that inherits from basegeometry"""
