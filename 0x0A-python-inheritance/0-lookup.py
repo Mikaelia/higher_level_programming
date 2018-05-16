c261f552abfd3c5897a01c2fdc9777ab79eda9ed
Mikaelia <mikaelagurney@gmail.com>
2018-05-16 14:38:45 +0000

    edit task 0 and 2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """ A function that returns the list of available attributes and methods of an object
+    """ Returns the list of attributes and methods of an object
         Returns: A list object
     """
     return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 def is_same_class(obj, a_class):
     """Checks if object is exactly same as specified class
         Return: True if true, else False"""
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) == a_class
