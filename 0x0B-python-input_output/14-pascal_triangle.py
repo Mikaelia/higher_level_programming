545411d8167f57010a6c4e441b88e7d0d6e16fde
Mikaelia <mikaelagurney@gmail.com>
2018-05-17 17:20:12 +0000

    update 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -5,16 +5,16 @@ def pascal_triangle(n):
         n [int]: Size of the triangle
     """
     if n <= 0:
-        return [[]]
+        return []
 
-    matrix = []
+    arrays = []
     for row in range(1, n + 1):
         new = []
         for i in range(row):
             if i == 0 or i == row - 1:
                 new.append(1)
             else:
-                new.append(matrix[row - 2][i] + matrix[row - 2][i - 1])
-        matrix.append(new)
+                new.append(arrays[row - 2][i] + arrays[row - 2][i - 1])
+        arrays.append(new)
 
-    return matrix
+    return arrays
