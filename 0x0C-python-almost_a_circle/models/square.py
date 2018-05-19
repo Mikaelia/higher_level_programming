8047dc7602f2c5184c09dc5b6f94f97b6d19e9e7
Mikaelia <mikaelagurney@gmail.com>
2018-05-19 19:51:54 +0000

    Add square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 """square module"""
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 class Square(Rectangle):
-    """A Square class"""
+    """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Square class constructor
+        """Square constructor
             Args:
-                size(int): size of square
-                x(int): position offset
-                y(int): position offset
+                size(int): size of the square
+                x(int): positional offset
+                y(int): positional offset
                 id(int): instance id
         """
         super().__init__(size, size, x, y, id)
+
     def __str__(self):
-        """overloads str method"""
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height))
+        "to string for square"
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        'size getter'
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        "size setter"
+        self.width = value
+        self.height = value
+
