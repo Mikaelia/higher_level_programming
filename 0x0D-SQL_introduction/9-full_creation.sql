bfa92c61b08be9d0d5ecbb82b747081ef0e06dfd
Mikaelia <mikaelagurney@gmail.com>
2018-06-25 23:00:09 +0000

    Remove auto increment

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,5 @@
 -- Creates a table second_table
-CREATE TABLE if not EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(256), score INT);
+CREATE TABLE if not EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score)
-VALUES (NULL, "John", '10'), (NULL, "Alex", 3), (NULL, "Bob", 14), (NULL, "George", 8);
+VALUES (1, "John", '10'), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
