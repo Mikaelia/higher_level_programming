9e79d3b952b684868428ce4f51b5df5e6aeb9568
Mikaelia <mikaelagurney@gmail.com>
2018-06-25 23:47:09 +0000

    Add task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score in the table second_table
+-- Displays the score, number of records for this score
+-- Sorted by the number of records (descending)
+SELECT score, count(*) as number from second_table
+group by score having number > 1
+order by score desc
