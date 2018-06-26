a938e0fc89b06cd3c31d276f39ba40066e87c5a3
Mikaelia <mikaelagurney@gmail.com>
2018-06-26 00:07:06 +0000

    Add task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all records of the table second_table
+-- Doesn't list rows without a name value
+-- Displays the score and the name
+-- Records listed by descending score
+SELECT score, name FROM second_table ORDER BY score DESC;
