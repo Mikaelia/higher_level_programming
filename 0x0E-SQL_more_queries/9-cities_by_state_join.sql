e5d6d32b0fa28fa20eaff69287bc758549872c36
Mikaelia <mikaelagurney@gmail.com>
2018-06-26 22:30:29 +0000

    Add task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Displays all cities in the database hbtn_0d_usa
-SELECT cities.id, states.name, cities.name FROM states INNER JOIN states ON cities.id=state.state_id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.id=state.state_id;
