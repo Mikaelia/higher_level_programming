3af425e779d7e67ae7e931ebdfd2c28b3a7647eb
Mikaelia <mikaelagurney@gmail.com>
2018-06-26 22:36:04 +0000

    Add missing 's'

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Displays all cities in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states ON cities.state_id=state.id
+FROM cities INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
