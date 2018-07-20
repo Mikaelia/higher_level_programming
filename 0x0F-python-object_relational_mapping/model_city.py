d070079ea926e5a0f7fc1e8886c03fc0e6504f8d
Mikaelia <mikaelagurney@gmail.com>
2018-07-20 17:27:13 +0000

    Import correct module

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,13 +5,12 @@ Creates schema for city class
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 
+Base = declarative_base()
 
-if __name__ == '__main__':
-    Base = declarative_base()
-
-    class City(Base):
-        __tablename__ = 'cities'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128), nullable=False)
-        state_id = Column(Integer, ForeignKey('states.id'))
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
