927ea5e61b7f5db65a120404972ee45759cad2c3
Mikaelia <mikaelagurney@gmail.com>
2018-07-20 06:16:29 +0000

    Pep8 compliance

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" A python file that contains the class definition of a State and an instance Base = declarative_base()
+""" Class definition of a State and an instance Base = declarative_base()
 """
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
