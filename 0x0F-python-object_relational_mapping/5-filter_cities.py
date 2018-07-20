92e51311844d9b58d00191d601912261dc59ed82
Mikaelia <mikaelagurney@gmail.com>
2018-07-20 06:13:34 +0000

    Pep8 compliance

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,7 +5,12 @@ if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name \
                 FROM states \
