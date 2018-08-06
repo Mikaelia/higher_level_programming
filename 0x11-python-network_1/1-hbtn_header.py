8efdbd60197ce2c1fb83f37512a3c4003bfaee19
Mikaelia <mikaelagurney@gmail.com>
2018-08-06 21:18:51 +0000

    Add name=main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """
 Sends a request to a URL, displays the value of the X-Request-Id variable
 """
-import urllib.request
-import sys
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    content = response.info()
-    print(content.get('X-Request-Id'))
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        content = response.info()
+        print(content.get('X-Request-Id'))
