d1a0bd884281423dd178dba23c28254afa17f05b
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 03:33:31 +0000

    remove 'with'

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,5 @@ Fetches https://intranet.hbtn.io/status
 if __name__ == '__main__':
     import requests
 
-    with requests.get('https://intranet.hbtn.io/status') as r:
-        print('Body response:\n\t- type: {}\n\t- content: {}'.
-              format(type(r.text), r.text))
+    text = requests.get('https://intranet.hbtn.io/status').text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(text), text))
