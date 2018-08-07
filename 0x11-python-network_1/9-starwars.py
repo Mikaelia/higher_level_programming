e79536d003e3621f9bc4298763883283367635f6
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 05:16:40 +0000

    Add task 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -8,9 +8,11 @@ import sys
 if __name__ == '__main__':
     val = sys.argv[1] if len(sys.argv) > 1 else ''
     values = {'search': val}
-    response = requests.get(
-        'https://swapi.co/api/people/',
-        params=values).json()
-    print('Number of results: {}'.format(response['count']))
-    for result in response['results']:
-        print (result['name'])
+    try:
+        response = requests.get('https://swapi.co/api/people/',
+            params=values).json()
+        print('Number of results: {}'.format(response['count']))
+        for result in response['results']:
+            print (result['name'])
+    except ValueError:
+        print("Must be JSON")
