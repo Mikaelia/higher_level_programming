5cef89975e8ec67fc75ef115ec99c02491ea63bf
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 03:40:08 +0000

    Add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Displays the value of the variable X-Request-Id in the response header
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    print(requests.post(sys.argv[1], data = {'email':sys.argv[2]}))
