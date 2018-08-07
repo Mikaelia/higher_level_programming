cb7bd9b577d1485f35a56e9c3ef6373a694b267d
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 05:38:58 +0000

    Add task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,8 @@ import sys
 
 if __name__ == '__main__':
     payload = (sys.argv[1], sys.argv[2])
-    response = requests.get('https://api.github.com/user/', auth=payload).json()
+    response = requests.get('https://api.github.com/user/',
+                            auth=payload).json()
     if 'id' in response:
         print(response['id'])
     else:
