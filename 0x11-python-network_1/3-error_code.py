07aafbde991ffba90b38e990ac64de3a879b0776
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 02:59:36 +0000

    Add task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Catches http errors
+Submits a post requeset with a specific email
 """
 import urllib.request
 import sys
@@ -9,8 +9,7 @@ if __name__ == '__main__':
     url = sys.argv[1]
     req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(req) as response:
-            data = response.read()
-            print(data.decode('utf-8'))
+        urllib.request.urlopen(req)
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
+    print(req.decode('utf-8'))
