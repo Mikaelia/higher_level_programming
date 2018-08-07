e7a938c7f707aae8660f14c7c1367577111833e8
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 03:17:10 +0000

    Add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,9 +3,7 @@
 Fetches https://intranet.hbtn.io/status
 """
 if __name__ == '__main__':
-    import urllib.request
+    import requests
 
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        data = response.read().decode('utf-8')
-        print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(data), data))
+    with requests.get('https://intranet.hbtn.io/status') as r:
+        print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(r), r))
