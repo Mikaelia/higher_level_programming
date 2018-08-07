fbe316af3763c197ba045f8fb8a87c07e0afc4f3
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 17:22:44 +0000

    Edit task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-Catches http errors
+    Sends and email via post
 """
-import urllib.request
-import sys
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(req) as response:
-            data = response.read()
-            print(data.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print('Error code: {}'.format(e.code))
+    import urllib.request
+    import utllib.parse
+    import sys
+
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('utf-8')
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode(encoding='utf-8'))
