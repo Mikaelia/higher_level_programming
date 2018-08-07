36f268a92cbd728200c8b8b2a04af1c8aba98afd
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 05:38:22 +0000

    Add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Queries github API
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    payload = (sys.argv[1], sys.argv[2])
+    response = requests.get('https://api.github.com/user/', auth=payload).json()
+    if 'id' in response:
+        print(response['id'])
+    else:
+        print('None')
