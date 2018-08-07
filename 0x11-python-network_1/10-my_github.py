d0fdb7cb497d7d779a5a7d0870bc2680f2077d53
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 17:55:58 +0000

    Edit 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,8 +8,9 @@ import sys
 if __name__ == '__main__':
     url = 'https://api.github.com/user/'
     payload = {'login': sys.argv[1]}
-    response = requests.get(url, params=payload,
-                            auth=(sys.argv[1], sys.argv[2])).json()
+    response = requests.get(
+        url, params=payload, auth=(
+            sys.argv[1], sys.argv[2])).json()
     try:
         print(response['id'])
     except KeyError:
