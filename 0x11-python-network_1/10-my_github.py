bdd6fb485f10812741ca25127eed1e33032ae872
Mikaelia <mikaelagurney@gmail.com>
2018-08-07 18:00:47 +0000

    Edit 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,12 +2,15 @@
 """
 Queries github API
 """
-import requests
 import sys
-if __name__ == "__main__":
-    user = requests.get('https://api.github.com/user',
-                        auth=(argv[1], argv[2])).json()
-    if "id" in user:
-        print(user['id'])
-    else:
-        print(None)
+import requests
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    p = {'login': sys.argv[1]}
+    r = requests.get(url, params={'login': sys.argv[1]},
+                     auth=(sys.argv[1], sys.argv[2])).json()
+
+    try:
+        print(r['id'])
+    except KeyError:
+        print('None')
