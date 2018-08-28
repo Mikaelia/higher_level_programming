25f7b7d44d8ac788303bc5fdace7b47ed0e6ca72
Mikaelia <mikaelagurney@gmail.com>
2018-08-28 22:41:10 +0000

    Add zero cases

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,6 +7,9 @@ process.argv.forEach(function (element, index) {
   }
 });
 
-myArray.sort(function (a, b) { return a - b; });
-
-console.log(myArray[myArray.length - 2]);
+if (myArray.length === 0 || myArray.length === 1) {
+  console.log(0);
+} else {
+  myArray.sort(function (a, b) { return a - b; });
+  console.log(myArray[myArray.length - 2]);
+}
