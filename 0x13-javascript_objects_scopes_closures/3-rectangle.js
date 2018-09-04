6eb2cc53796a69b5e4472de6cfc5ed48cc864bec
Mikaelia <mikaelagurney@gmail.com>
2018-09-04 21:29:07 +0000

    Add semicolons

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,11 +4,11 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    }
-  }
-  print () {
-    for (var i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
+    };
+  };
+    print() {
+      for (var i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+    };
+  };
 };
