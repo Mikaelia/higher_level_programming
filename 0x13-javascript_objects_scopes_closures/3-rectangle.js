65f020363cc2c1be868f254da9e53cc8fd47bc97
Mikaelia <mikaelagurney@gmail.com>
2018-09-04 21:31:30 +0000

    change var to let

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,11 +4,12 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    };
-  };
-    print() {
-      for (var i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-    };
-  };
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 };
