9fa75cea9e16e0ad1b7703a0365bd2d60c381d19
Mikaelia <mikaelagurney@gmail.com>
2018-09-04 21:12:11 +0000

    Change order of props

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      this.height = h;
       this.width = w;
-    } else {
-      Object.create(Rectangle);
+      this.height = h;
     }
   }
 };
