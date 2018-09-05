61c915a31215129d985b2b64b2929864807b72f4
Mikaela Gurney <354@holbertonschool.com>
2018-09-05 15:09:18 -0700

    remove hard-coded object

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,24 +8,18 @@ request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let taskCount = {
-      '1': 0,
-      '2': 0,
-      '3': 0,
-      '4': 0,
-      '5': 0,
-      '6': 0,
-      '7': 0,
-      '8': 0,
-      '9': 0,
-      '10': 0
-    };
+    let taskCount = {};
     const taskList = JSON.parse(body);
     for (const task of taskList) {
       if (task.completed === true) {
-        taskCount[task.userId] += 1;
-      }
+        let id = task.userId;
+        if (taskCount[id] === undefined) {
+          taskCount[id] = 1;
+        } else {
+          taskCount[id] += 1;
+        }
     }
-    console.log(taskCount);
   }
+  console.log(taskCount);
+}
 });
