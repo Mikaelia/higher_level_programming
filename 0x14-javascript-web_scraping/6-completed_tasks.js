c2ae9ce55efdf3965b1dfb4df3ddb6c05286535e
Mikaela Gurney <354@holbertonschool.com>
2018-09-05 14:49:11 -0700

    Add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    var taskCount = {
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0,
+      '6': 0,
+      '7': 0,
+      '8': 0,
+      '9': 0,
+      '10': 0
+    };
+    const taskList = JSON.parse(body);
+    for (const task of taskList) {
+      if (task.completed === true) {
+        taskCount[task.userId] += 1;
+      }
+    }
+    console.log(taskCount);
+  }
+});
